fe434aad364fcc5aa1d5ad4e3111790488994b18
M9 <9yu6820@gmail.com>
2023-04-18 10:40:54 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>오늘의 할일</h1>
+    <input v-model="searchText" type="text" class="form-control" placeholder="검색어를 입력하세요" />
+    <TodoBasicForm @add-todo="onSubmit" />
+    <div v-if="!todos.length">등록된 일정이 없습니다</div>
+    <TodoList :todos="filteredTodos" @toggle-todo="toggleTodo" @delete-todo="deleteTodo" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref, computed } from "vue";
+import TodoBasicForm from "./components/TodoBasicForm.vue";
+import TodoList from "./components/TodoList.vue";
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TodoBasicForm,
+    TodoList,
+  },
+  setup() {
+    const toggle = ref(false);
+    const searchText = ref("");
+    const filteredTodos = computed(() => {
+      if (searchText.value === "") {
+        return todos.value.filter((todo) => {
+          console.log(todos.value, todo);
+          return todo.subject.includes(searchText.value);
+        });
+      }
+      return todos.value;
+    });
+    const todos = ref([]);
+    const onSubmit = (todo) => {
+      todos.value.push(todo);
+    };
+    const todoStyle = {
+      textDecoration: "line-through",
+      color: "gray",
+    };
+    const deleteTodo = (index) => {
+      console.log(index);
+      todos.value.splice(index, 1);
+    };
+    const toggleTodo = (index) => {
+      console.log(index);
+      todos.value[index].completed = !todos.value[index].completed;
+    };
+
+    return {
+      filteredTodos,
+      searchText,
+      onSubmit,
+      todos,
+      toggle,
+      todoStyle,
+      deleteTodo,
+      toggleTodo,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.todo {
+  color: gray;
+  text-decoration: line-through;
 }
 </style>
---
+++ src/components/TodoList1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <div class="card-body p-2">
+      <div class="d-flex" v-for="i in todos" :key="i.id">
+        <div class="form-check flex-grow-1">
+          <label class="form-check-label" :class="{ tolist: i.completed }"> <input type="checkbox" class="form-check-input" v-model="i.complated" />{{ i.subject }}</label>
+        </div>
+        <div>
+          <button class="btn btn-danger btn-sm" @click="deleteTodo(i)">삭제</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  //setup(props) {},
+};
+</script>
+
+<style></style>
